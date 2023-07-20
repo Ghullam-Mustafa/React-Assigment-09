1bb4af54246c005fb0f163fc0e402debbdda2e84
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-07-20 23:27:01 +0500

    try

---
+++ src/pages/MainPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+export default function MainPage() {
+  return (
+    <div className='text-center'>MainPage</div>
+  )
+}
