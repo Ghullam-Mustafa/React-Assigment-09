bc89f299ea76e699c12acefbcd64602071a66acf
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-07-20 23:27:31 +0500

    da

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,6 +3,6 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 export default function MainPage() {
   return (
-    <div className='text-center'>MainPage</div>
+    // <div className='text-center'>MainPage</div>
   )
 }
