e7407346c7b0162b701b55e02ad65b6ecbaaf536
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-07-22 01:40:23 +0500

    hero section

--- src/components/heroSection/HeroSection.js
+++ src/components/heroSection/HeroSection.js
@@ -9,37 +9,49 @@ export default function HeroSection() {
     return (
         <div className='heroSection'>
             <div className="container ">
-                <div className="row o">
+                <div className="row ">
                     <div className="col-lg-6 col-md-6 col-sm-12">
-                        <p className='p1'>Discover digital art & Collect NFTs</p>
-                        <p className='p2'>NFT marketplace UI created with Anima for Figma. Collect, buy and sell art from more than 20k NFT artists.</p>
+                        <p className='text-white h1 display-1 fw-bold'>Discover  Digital Art & Collect NFTs</p>
+                        <p className='text-white fs-4 font-monospace'>NFT Marketplace UI Created with Anima for Figma. Collect, Buy And Sell Art From More Than 20k NFT Artists.</p>
                         <ButtonOne />
                         <div className="row pt-3 d-flex">
-                            <div className="col-lg-4 col-sm-4 col-md-4">
+                            <div className="col-4 ">
                                 <Cards total="240" title="Total Sale" />
                             </div>
-                            <div className="col-lg-4 col-sm-4 col-md-4">
-                                <Cards total="100" title="Actions" />
+                            <div className="col-4 ">
+                                <Cards total="100" title="Auctions" />
                             </div>
-                            <div className="col-lg-4 col-sm-4 col-md-4">
+                            <div className="col-4 ">
                                 <Cards total="240" title="Artists" />
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-6 col-md-6 col-sm-12">
-                        <div className="">
-                            <div class="card" >
-                                <img src={img} class="card-img-top" alt="..."/>
-                                    <div class="card-body ">
-                                        <h5 className='text-white'>Space Walking</h5>
-                                        <div>
-                                            <img src={img2} className='img-fluid ' alt="" />
-                                            
-                                        </div>
-                                    </div>
+
+
+                    <div className="col-lg-6 col-md-6 col-sm-12   ">
+                        {/* <div className="card m-2 border rounded-3 d-flex justify-content-around" >
+                            <img src={img} className="card-img-top border-0" alt="..." />
+                            <div className="card-body ">
+                                <h5 classNameName='text-white'>Space Walking</h5>
+                                <div>
+                                    <img src={img2} classNameName='img-fluid ' alt="" />
+
+                                </div>
+                            </div>
+                        </div> */}
+                        <div className="card border-0 cards" >
+                            <img src={img} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                            <h5 classNameName='text-white'>Space Walking</h5>
+                                <div>
+                                    <img src={img2} classNameName='img-fluid ' alt="" />
+
+                                </div>
                             </div>
                         </div>
                     </div>
+
+
                 </div>
             </div>
         </div>
--- src/components/heroSection/card/Cards.js
+++ src/components/heroSection/card/Cards.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import  '../../heroSection/card/Card.css'
 
 export default function Cards(props) {
   return (
     <div >
-        <h5 className='total'>{props.total}k+</h5>
-        <p className='text-white titl'>{props.title}</p>
+        <p className='total text-white h6 display-6 fw-bold '>{props.total}k+</p>
+        <p className='text-white titl text-white fs-4  fw-light'>{props.title}</p>
     </div>
   )
 }
