2ce6758554fc1f863c57383be3ab47d5ce014e2f
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-07-23 02:19:55 +0500

    footer complete

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,18 +9,18 @@ export default function Footer() {
         <div className="container-fluid fl">
             <div className="container pt-5">
                 <div className="row">
-                    <div className="col-lg-4 col-md-12 col-sm-12">
+                    <div className="col-lg-4 col-md-12 col-sm-12 my-3">
                         <img src={img} alt="" />
                         <p className='sales'>NFT marketplace UI creater <br /> with Anima for Figma.</p>
                         <p className='sales'>Join our community</p>
                     </div>
-                    <div className="col-lg-4 col-md-12 col-sm-12">
+                    <div className="col-lg-4 col-md-12 col-sm-12 my-3">
                         <h6 className='text-white'>Explore</h6>
                         <p className='sales'>Marketplace</p>
                         <p className='sales'>Rankings</p>
                         <p className='sales'>Connect a wallet</p>
                     </div>
-                    <div className="col-lg-4 col-md-12 col-sm-12">
+                    <div className="col-lg-4 col-md-12 col-sm-12 my-3">
                     <h6 className='text-white'>Join Our Weekly Digest</h6>
                         <p className='sales'>Get exclusive promotions & updates <br />straight in your box</p>
                         <SubscribeBtn/>
