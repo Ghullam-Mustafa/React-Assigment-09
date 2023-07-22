5ef4adb21b3271d77aabd506b019beac947db109
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-07-22 13:27:52 +0500

    brwsCollection

--- src/components/browsCategories/BrowsCategories.js
+++ src/components/browsCategories/BrowsCategories.js
@@ -21,32 +21,32 @@ export default function BrowsCatagories() {
                     </div>
                 </div>
 
-                <div className="row py-3 d-flex justify-content-sm-center ">
-                    <div className="col-lg-3 col-md-6 col-sm-12 p-2">
+                <div className="row py-3 align-items-stretch ">
+                    <div className="col-lg-3 col-md-3 col-sm-6 p-2">
                         <BrowsCards src={img1} name="Art"/>
                     </div>
-                    <div className="col-lg-3 col-md-3 col-sm-12 p-2">
+                    <div className="col-lg-3 col-md-3 col-sm-6 p-2">
                         <BrowsCards src={img2} name="Collectibles"/>
                     </div>
-                    <div className="col-lg-3 col-md-3 col-sm-12 p-2">
+                    <div className="col-lg-3 col-md-3 col-sm-6 p-2">
                         <BrowsCards src={img3} name="Music"/>
                     </div>
-                    <div className="col-lg-3 col-md-3 col-sm-12 p-2">
+                    <div className="col-lg-3 col-md-3 col-sm-6 p-2">
                         <BrowsCards src={img4} name="Photography"/>
                     </div>
                 </div>
 
-                <div className="row d-flex justify-content-sm-center">
-                    <div className="col-lg-3 col-md-6 col-sm-12 p-2">
+                <div className="row d-flex align-items-stretch">
+                    <div className="col-lg-3 col-md-3 col-sm-6 p-2">
                         <BrowsCards src={img5} name="Video"/>
                     </div>
-                    <div className="col-lg-3 col-md-3 col-sm-12 p-2">
+                    <div className="col-lg-3 col-md-3 col-sm-6 p-2">
                         <BrowsCards src={img6} name="Utility"/>
                     </div>
-                    <div className="col-lg-3 col-md-3 col-sm-12 p-2">
+                    <div className="col-lg-3 col-md-3 col-sm-6 p-2">
                         <BrowsCards src={img7} name="Sport"/>
                     </div>
-                    <div className="col-lg-3 col-md-3 col-sm-12 p-2">
+                    <div className="col-lg-3 col-md-3 col-sm-6 p-2">
                         <BrowsCards src={img8} name="Virtual Worlds"/>
                     </div>
                 </div>
--- src/components/browsCategories/browsCards/BrowsCards.js
+++ src/components/browsCategories/browsCards/BrowsCards.js
@@ -3,24 +3,24 @@ import '../browsCards/BrowsCards.css'
 
 export default function BrowsCards(props) {
     return (
-        
-            <div className=" rounded-5 border-0 bg-transparent">
 
-                <div className=" ">
+        <div className=" rounded-5 border-0 bg-transparent">
 
-                    {/* <img src={props.src} alt="" className='img-fluid'/>
+            <div className=" ">
+
+                {/* <img src={props.src} alt="" className='img-fluid'/>
                 
                 <div className="p-3 clr">
                 <h4 className='card-subtitle mb-2 text-white'>{props.name} </h4> */}
-                    {/* </div> */}
-                    <div class="card bg-transparent border-0" style={{width: '16rem'}}>
-                        <img src={props.src} class="card-img-top" className='img-fluid' alt="..."/>
-                            <div class="card-body">
-                                <h4 class="card-text text-white px-4">{props.name}</h4>
-                            </div>
+                {/* </div> */}
+                <div class="card bg-transparent border-0" >
+                    <img src={props.src} class="card-img-top" className='img-fluid' alt="..." />
+                    <div class="card-body">
+                        <h4 class="card-text text-white px-1">{props.name}</h4>
                     </div>
                 </div>
             </div>
-        
+        </div>
+
     )
 }
